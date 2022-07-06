137a5e5ca9e0220befffea4a9c5b40d2d7a1e55f
Leonhardtjs <michael.amv30@gmail.com>
2022-07-06 16:38:27 -0500

    Add pipeline github actions

--- scripts/application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'run application_start.sh: ' >> /home/ec2-user/tbg-backend/deploy.log
-
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/tbg-backend/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/tbg-backend/deploy.log
