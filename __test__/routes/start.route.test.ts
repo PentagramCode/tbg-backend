f98f5b36a8402e0f843386f1b88dff4f1dc5c5b5
Leonhardtjs <michael.amv30@gmail.com>
2022-07-19 15:15:36 -0500

    Resolved comments

--- __test__/routes/start.route.test.ts
+++ __test__/routes/start.route.test.ts
@@ -7,7 +7,6 @@ import Server from '../../src/servers/server';
 const server = new Server();
 
 describe('Testing start route', () => {
-    
     test('should respond with a 200 status code', async() => {
         const response = await request(server.app).get('/health').send();
         expect(response.statusCode).toBe(200);
